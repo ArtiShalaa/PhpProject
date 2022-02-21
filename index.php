09e1ee62dfb27aee870f0424035c3ff403dc32f4
Your Name <90637526+ArtiShalaa@users.noreply.github.com>
2022-02-21 12:31:47 +0100

    Update

--- index.php
+++ index.php
@@ -127,8 +127,7 @@ if(isset($_POST['submit'])){
   <img src="./images-tour/g-9.jpg" style="width:100%">
 </div>
 
-<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-<a class="next" onclick="plusSlides(1)">&#10095;</a>
+
 </div>
 <br>
 
